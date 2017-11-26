58687c1254674d551b719bfa92d40306c4adb518
Morayg <moraygg@gmail.com>
2017-11-27 01:03:52 +0300

    dfdfgsdfg

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ bot.onText(/\/allnames (.*)/, (msg, match) => {
           var collection = db.collection('names');
           collection.find({}).toArray(function(err, result) {
                if (err) throw err;
-               for (var i; i < result.length; i++) {
+               for (var i = 0; i < result.length; i++) {
                bot.sendMessage(chatId, result[i]);
                //console.log(result);
                };
