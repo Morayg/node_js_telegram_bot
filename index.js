0ab974b1ef1459af3412e6d23df20336084d3383
Morayg <moraygg@gmail.com>
2018-01-14 12:54:11 +0300

    add edit note

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
 var MongoClient = require('mongodb').MongoClient;
+var mongo = require('mongodb');
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/tb1';
 var all_value = /./;
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '428346731:AAG02e5MXsewWGMraXDKDJJlSGoyJOhXYzc';
+const token = '489189460:AAEfNRAinVtaxR4_VYoe3x6sxIfJMcY-mlY';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
@@ -52,6 +53,7 @@ bot.onText(/\/show10menotes/, (msg, match) => {
 	var result_value;
 
 	function show10notes() {
+		if (skipdecade < 0) {skipdecade = 0};
 		MongoClient.connect(url, (err, db) => {
 			assert.equal(null, err);
 			var collection = db.collection('notes');
@@ -59,8 +61,16 @@ bot.onText(/\/show10menotes/, (msg, match) => {
 				result_value = result.length;
 				if (err) throw err;
 				bot.sendMessage(chat_id, ('Ваши заметки с ' + (skipdecade + 1) + ' по ' + (skipdecade + result_value) + ': '),{reply_markup: {keyboard: [['/next', '/previous']]}});
+				listener_pagenator();
 				for (i = 0; i < result.length; i++) {
-					bot.sendMessage(chat_id, result[i]['note_body']);
+					var keyboard = {
+						reply_markup: JSON.stringify({
+						  	inline_keyboard: [
+								  [{text: "Изменить текст заметки", callback_data: 'edit_note' + ';;' + result[i]['_id']}]
+							]
+						})
+					};
+					bot.sendMessage(chat_id, result[i]['note_body'], keyboard);
 				};
 			});
 		});
@@ -69,91 +79,55 @@ bot.onText(/\/show10menotes/, (msg, match) => {
 	//Выводит первую страницу и меню
 	show10notes();
 
-	bot.onText(next_notes, (msg,match) => {
-		skipdecade = skipdecade + 10;
-		show10notes();
-		bot.removeTextListener(next_notes);
-	});
-	bot.onText(previous_notes, (msg,match) => {
-		skipdecade = skipdecade - 10;
-		show10notes();
-		bot.removeTextListener(previous_notes);
-	});
+	function listener_pagenator() {
+		bot.onText(next_notes, (msg,match) => {
+			skipdecade = skipdecade + 10;
+			bot.removeTextListener(next_notes);
+			bot.removeTextListener(previous_notes);
+			show10notes();
+		});
+		bot.onText(previous_notes, (msg,match) => {
+			skipdecade = skipdecade - 10;
+			bot.removeTextListener(next_notes);
+			bot.removeTextListener(previous_notes);
+			show10notes();
+		});
+	};
 });
 
-/*
-
-note_id
-user_id
-note_created_date
-note_update_date
-note_folder_name
-note_head
-note_body
-note_attach
-note_archived_date
-
-bot.onText(/\/allnames/, (msg, match) => {
-     var chatId = msg.chat.id;
-     var resp = match[1];
-     MongoClient.connect(url, function(err, db) {
-          assert.equal(null, err);
-          var collection = db.collection('names');
-          collection.find({}).toArray(function(err, result) {
-               if (err) throw err;
-               for (var i = 0; i < result.length; i++) {
-               var message = result[i]['name'] + ' - ' + result[i]['chatId'];
-               bot.sendMessage(chatId, message);
-               //console.log(result[i].toString);
-               };
-          db.close();
-          });
-     });
+bot.on('callback_query', function (msg) {
+	var answer = msg.data.split(';;');
+	var callback_type = answer[0];
+	var id_note = new mongo.ObjectID(answer[1]);
+	//console.log(id_note);
+	if (callback_type == 'edit_note') {
+		//то запускаем редактирование
+		try {
+			MongoClient.connect(url, (err, db) => {
+				assert.equal(null, err);
+				var collection = db.collection('notes');
+					collection.findOne({_id: id_note}, function(err, message) {
+						//console.log(message);
+						var chat_id = message.chat_id;
+						bot.sendMessage(chat_id, 'Отправьте новый текст заметки: ' + message.note_body);
+						bot.onText(all_value, (msg, match) => {
+							var chat_id = msg.chat.id;
+							var user_id = msg.from.id;
+							var note_edited_date = msg.date;
+							var note_body = msg.text;
+							MongoClient.connect(url, function(err, db) {
+								assert.equal(null, err);
+								var collection = db.collection('notes');
+								collection.update({_id:  id_note}, {user_id: user_id, chat_id: chat_id, note_edited_date: note_edited_date, note_body: note_body});
+								bot.sendMessage(chat_id, 'Заметка отредактирована!');
+								db.close();
+							});
+							bot.removeTextListener(all_value);
+						});
+					});
+			});
+		} catch (err) {
+			console.log(err);
+		};
+	};
 });
-
-bot.onText(/\/find (.*)/, (msg, match) => {
-     var chatId = msg.chat.id;
-     var resp = match[1];
-     MongoClient.connect(url, function(err, db) {
-          if (err) throw err;
-          assert.equal(null, err);
-          var collection = db.collection('names');
-          var answer = collection.findOne({name: resp}, function(err, result) {
-            if (err) throw err;
-            bot.sendMessage(chatId, result['name'] + ' - ' + result['chatId']);
-            db.close();
-          });
-     });
-});
-
-bot.onText(/\/delete (.*)/, (msg, match) => {
-     var chatId = msg.chat.id;
-     var resp = match[1];
-     MongoClient.connect(url, function(err, db) {
-          if (err) throw err;
-          assert.equal(null, err);
-          var collection = db.collection('names');
-          var answer = collection.deleteOne({name: resp}, function(err, result) {
-            if (err) throw err;
-            bot.sendMessage(chatId, 'delete:' + result['name'] + ' - ' + result['chatId']);
-            db.close();
-          });
-     });
-});
-
-
-bot.onText(/\/check_attribute (.*)/, (msg, match) => {
-     var chatId = msg.chat.id;
-     var resp = match[1];
-     for (i in msg) {
-      bot.sendMessage(chatId, i + ' ' + msg[i])
-        if (typeof(msg[i]) == 'object') {
-          for (u in msg[i]) {
-            bot.sendMessage(chatId, i + ' ' + u + ' ' + msg[i][u]);
-          };
-        };    
-     };
-});
-
-
-*/
